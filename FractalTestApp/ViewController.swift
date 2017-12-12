3df19318f6381d3a57452fb3c62280420a46a417
jonathalima <devjonh@gmail.com>
2017-12-11 21:43:02 -0300

    Initial Commit

---
+++ FractalTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FractalTestApp
+//
+//  Created by Jonatha Lima on 11/12/17.
+//  Copyright © 2017 Jlima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
